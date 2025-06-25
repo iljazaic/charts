1513de154b8fc1b2da5bfa3e1c251599fedd8af8
Ilja Zaicev <iljazaicevDK@outlook.com>
2025-06-25 03:28:49 +0200

    Belize graphy and other:

--- server/server.js
+++ server/server.js
@@ -9,67 +9,86 @@ app.use(express.static(__dirname.replace('server', 'public')));
 async function compileCrossectionData(country) {
     const data = fs.readFileSync('./topography/crossection3.csv', 'utf-8');
     const lines = data.split('\n');
-    const package = {
-        'Distances':[],
-        'Elevations':[]
+    var package = {
+        'Distances': [],
+        'Elevations': []
     }
     for (let i = 0; i < lines.length; i++) {
         const countryLine = lines[i].split(',')[0];
-        if(country==countryLine || country.toLowerCase()==countryLine.toLowerCase() || countryLine.toLowerCase().includes(country.toLowerCase())){
+        if (country == countryLine || country.toLowerCase() == countryLine.toLowerCase() || countryLine.toLowerCase().includes(country.toLowerCase())) {
             const elevationData = lines[i].split(',');
-            var distanceData = lines[i+1].split(',');
+            var distanceData = lines[i + 1].split(',');
             elevationData.shift();
             distanceData.shift();
 
-            for (let j = 0; j<distanceData.length; j++){
+            for (let j = 0; j < distanceData.length; j++) {
                 let d = Math.round(parseFloat(distanceData[j]));
-                if (d<=0){
-                    distanceData[j]=d.toString()+'km-W'
-                }else{
-                    distanceData[j]=d.toString()+'km-E'
+                if (d <= 0) {
+                    distanceData[j] = d.toString() + 'km-W'
+                } else {
+                    distanceData[j] = d.toString() + 'km-E'
                 }
             }
             package.Distances = distanceData;
             package.Elevations = elevationData;
             return package;
-            break;
         }
     }
 
 }
 
 
-async function getNewDataset(country){
+async function compileDietData(country) {
+    const data = fs.readFileSync('./server/datasets/OurWorldInData/dietaryData.csv', 'utf-8');
+    const lines = data.split('\n');
+
+    var package = [];
+    const valueArray = lines[0].split(',')
+    for (let i = 0; i < lines.length; i++) {
+        const countryLine = lines[i].split(',')[0];
+        if (country == countryLine || country.toLowerCase() == countryLine.toLowerCase() || countryLine.toLowerCase().includes(country.toLowerCase())) {
+            const dietData = lines[i].split(',');
+            for (let j = 2; j < dietData.length; j++) {
+                package.push({ name: valueArray[j], value: parseFloat(dietData[j]) })
+            }
+            return package;
+        }
+    }
+
+}
+
+async function getNewDataset(country) {
     return 'crossection'
 }
 
 
-app.get("/getNewDataset", async(req, res)=>{
+app.get("/getNewDataset", async (req, res) => {
     const country = req.query.c;
 
     //template to send back?
     const package = {
-        'chartType':'',
-        'data':[]
+        'chartType': '',
+        'data': []
     }
 
     const chartType = await getNewDataset(country);
 
     package.chartType = chartType;
 
-    switch(chartType){
+    switch (chartType) {
         case 'crossection':
-            console.log('got ere')
             package.data = await compileCrossectionData(country);
             break;
+        case 'diet':
+            package.data = await compileDietData(country);
     }
     res.send(package);
 })
 
-app.get("/", async(req, res)=>{
+app.get("/", async (req, res) => {
     res.sendFile('C:/Users/iljaz/desktop/charts_game/public/index.html')
 })
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
     console.log("user connected");
 })
--- topography/topography.py
+++ topography/topography.py
@@ -5,68 +5,68 @@ import math
 
 def createCrossection():
     with open('capitals.csv', 'r') as capitals:
-        for line in capitals:
-            try:
-                lat = round(float(line.split(',')[2]),5)
-                lon = round(float(line.split(',')[3]),5)
-                step = 0.001
+        try:
+            for line in capitals:
+                try:
+                    lat = round(float(line.split(',')[2]),5)
+                    lon = round(float(line.split(',')[3]),5)
+                    step = 0.001
 
-                lon_init = lon
+                    lon_init = lon
 
-                unknown_tolerancy = 5000
-                current_unknown = 0
-                stepInKm = step * 111.32*math.cos(math.radians(lat))
-                initialCountry = requests.get('http://localhost:5000/countryAtCoord', params={'lat':lat,'lon':lon}).json()['country']
-                currentCountry = initialCountry
-                #go east
-                eastData = []
-                while(currentCountry==initialCountry or current_unknown<=unknown_tolerancy and currentCountry=='Unknown'):
-                    lon+=step
-                    
-                    currentCountry = requests.get('http://localhost:5000/countryAtCoord', params={'lat':lat,'lon':lon}).json()['country']
-                    if currentCountry=='Unknown' or current_unknown>0 and currentCountry!=initialCountry:
-                        print(lat, lon)
-                        current_unknown+=1
-                    else:
-                        current_unknown=0
-                    elevation = round(float(requests.get('http://localhost:5000/v1/etopo1', params={'locations':str(lat)+','+str(lon)}).json()['results'][0]['elevation']),0)
-                    eastData.append(elevation)
-                currentCountry = initialCountry
-                if current_unknown>0:
-                    del eastData[-current_unknown:]
-                current_unknown=0
-                step=0.001
-                print('GO WEST')
-                westData = []
-                lon = lon_init
+                    unknown_tolerancy = 5000
+                    current_unknown = 0
+                    stepInKm = step * 111.32*math.cos(math.radians(lat))
+                    initialCountry = requests.get('http://localhost:5000/countryAtCoord', params={'lat':lat,'lon':lon}).json()['country']
+                    currentCountry = initialCountry
+                    #go east
+                    eastData = []
+                    while(currentCountry==initialCountry or current_unknown<=unknown_tolerancy and currentCountry=='Unknown'):
+                        lon+=step
 
-                while(currentCountry==initialCountry or current_unknown<=unknown_tolerancy and currentCountry=='Unknown'):
-                    lon-=step
-                    currentCountry = requests.get('http://localhost:5000/countryAtCoord', params={'lat':lat,'lon':lon}).json()['country']
+                        currentCountry = requests.get('http://localhost:5000/countryAtCoord', params={'lat':lat,'lon':lon}).json()['country']
+                        if currentCountry=='Unknown' or current_unknown>0 and currentCountry!=initialCountry:
+                            current_unknown+=1
+                        else:
+                            current_unknown=0
+                        elevation = round(float(requests.get('http://localhost:5000/v1/etopo1', params={'locations':str(lat)+','+str(lon)}).json()['results'][0]['elevation']),0)
+                        eastData.append(elevation)
+                    currentCountry = initialCountry
+                    if current_unknown>0:
+                        del eastData[-current_unknown:]
+                    current_unknown=0
+                    step=0.001
+                    westData = []
+                    lon = lon_init
 
-                    if currentCountry=='Unknown' or current_unknown>0 and currentCountry!=initialCountry:
-                        print(lat, lon)
-                        current_unknown+=1
-                    else:
-                        current_unknown=0
-                
-                    elevation = round(float(requests.get('http://localhost:5000/v1/etopo1', params={'locations':str(lat)+','+str(lon)}).json()['results'][0]['elevation']),0)
-                    westData.insert(0,elevation)
-                
-                if current_unknown>0:
-                    westData=westData[current_unknown:]
-                
-                fullList = westData+eastData
-                dw = (stepInKm*len(westData))*-1
-                de = (stepInKm*len(eastData))
-                distances = np.array(np.arange(dw, de, stepInKm))
-                csvFormElevation = ','.join(map(str, fullList)) 
-                csvFormDistances = ','.join(map(str, distances)) 
-                with open('crossection3.csv', 'a', newline='') as crossection:
-                    crossection.write(initialCountry+','+csvFormElevation+'\n')
-                    crossection.write(initialCountry+','+csvFormDistances+'\n')
-                    crossection.close()
-            except:
-                print(line.split(',')[0])
+                    while(currentCountry==initialCountry or current_unknown<=unknown_tolerancy and currentCountry=='Unknown'):
+                        lon-=step
+                        currentCountry = requests.get('http://localhost:5000/countryAtCoord', params={'lat':lat,'lon':lon}).json()['country']
+
+                        if currentCountry=='Unknown' or current_unknown>0 and currentCountry!=initialCountry:
+                            current_unknown+=1
+                        else:
+                            current_unknown=0
+
+                        elevation = round(float(requests.get('http://localhost:5000/v1/etopo1', params={'locations':str(lat)+','+str(lon)}).json()['results'][0]['elevation']),0)
+                        westData.insert(0,elevation)
+
+                    if current_unknown>0:
+                        westData=westData[current_unknown:]
+
+                    fullList = westData+eastData
+                    dw = (stepInKm*len(westData))*-1
+                    de = (stepInKm*len(eastData))
+                    distances = np.array(np.arange(dw, de, stepInKm))
+                    csvFormElevation = ','.join(map(str, fullList)) 
+                    csvFormDistances = ','.join(map(str, distances)) 
+                    with open('crossection3.csv', 'a', newline='') as crossection:
+                        crossection.write(initialCountry+','+csvFormElevation+'\n')
+                        crossection.write(initialCountry+','+csvFormDistances+'\n')
+                        crossection.close()
+                except:
+                    print(line.split(',')[0])
+        except:
+            print('oh no')
 createCrossection()
 
